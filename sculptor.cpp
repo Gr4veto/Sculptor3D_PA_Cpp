4a5ab1970d8915be86e17507b1accf21e7f38e7a
Moisés Medeiros <52640097+Moises788@users.noreply.github.com>
2022-06-28 00:03:24 -0300

    commit 03

--- sculptor.cpp
+++ sculptor.cpp
@@ -1,94 +1,60 @@
+#include "sculptor.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iomanip>
 #include <cmath>
 #include <vector>
-#include "sculptor.h"
 
 using namespace std;
 
 Sculptor::Sculptor(int _nx, int _ny, int _nz){
-    //Coletando coordenadas
-	nx = _nx;
-    ny = _ny;
-    nz = _nz;
-    
-    //Atribuindo valores iniciais
-    fix = 0.5;
-    r = 0.5;
-    g = 0.5; 
-    b = 0.5;
-    a = 0.5;
-
-    //AlocaÁ„o da matriz3d
+    nx = _nx; ny = _ny; nz = _nz;
+    r = 1; g= 1; b = 1;
+    // Aloca√ß√£o din√¢mica da matriz 3d
     v = new Voxel**[nx];
      for (int i =0; i<nx; i++){
       v[i] = new Voxel*[ny];
 
-     for (int j =0; j<ny; j++){
+        for (int j =0; j<ny; j++){
        v[i][j]= new Voxel[nz];
       }
     }
 }
 
-/*----------------------------------------------------------------*/
-
 Sculptor::~Sculptor(){
-//LiberaÁ„o da memÛria alocada	
     for(int i = 0; i < nx; i++){
         for (int j = 0; j < ny; j++){
-               delete[] v[i][j];
-            }
+            delete[] v[i][j];
         }
-        for (int i=0; i <nx; i++){
-            delete[] v[i];
-        }
+    }
+    for (int i=0; i <nx; i++){
+        delete[] v[i];
+    }
 
-        delete[] v;
-
-//Resetando coordenadas
-        nx = 0;
-        ny = 0;
-        nz = 0;
-		
-		}
-		
-/*----------------------------------------------------------------*/
-
-void Sculptor::setColor(float r_, float g_, float b_, float alpha) // define a cor e transparÍncia de cada voxel
-{
-	//O usu·rio define as propriedades do voxel
-    r = r_;
-    g = g_;
-    b = b_;
-    a = alpha;
+    delete[] v;
+    //Resetando coordenadas
+    nx = 0; ny = 0; nz = 0;
 }
 
-/*----------------------------------------------------------------*/
+void Sculptor::setColor(float r, float g, float b, float a){
+	//O usu√°rio define as propriedades do voxel
+    Sculptor::r = r; Sculptor::g = g; Sculptor::b = b; Sculptor::a = a;
+}
 
 void Sculptor::putVoxel(int x, int y, int z){
-	//insere um voxel
-    v[x][y][z].isOn = true;
+    //insere um voxel
+    v[x][y][z].isOn = true; 
     //repassa cor e transparencia do voxel
-    v[x][y][z].r = r;
-    v[x][y][z].g = g;
-    v[x][y][z].b = b;
-    v[x][y][z].a = a;    
+    v[x][y][z].r = r; v[x][y][z].g = g; v[x][y][z].b = b; v[x][y][z].a = a;    
 
 }
 
-/*----------------------------------------------------------------*/
-
 void Sculptor::cutVoxel(int x, int y, int z){
-	//Desativa o voxel
     v[x][y][z].isOn = false;
 }
 
-/*----------------------------------------------------------------*/
-
 void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
-    
     //Define uma sequencia de voxel
 	for(int x = x0; x<x1; x++){
        for (int y = y0; y<y1; y++){
@@ -103,10 +69,8 @@ void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
     }  
 }
 
-/*----------------------------------------------------------------*/
-
 void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
-	
+  	
 	//Desativa uma sequencia de voxel
     for(int x = x0; x<x1; x++){
        for (int y = y0; y<y1; y++){
@@ -114,10 +78,8 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
                v[x][y][z].isOn = false;
            }
        }
-    }
+    }  
 }
-
-/*----------------------------------------------------------------*/
 
 void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
 //Recebe as coordenadas e o raio da esfera
@@ -128,19 +90,16 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
       for(j = -radius; j<=radius; j++){
           for(k = -radius; k<=radius; k++){
          
-            if ((i*i+j*j+k*k) < radius*radius){ //condiÁ„o da esfera
+            if ((i*i+j*j+k*k) < radius*radius){ //condi√ß√£o da esfera
               putVoxel(i+xcenter,j+ycenter,k+zcenter); //adiciona um voxel
             }
           }
       }
   }
-
 }
 
-/*----------------------------------------------------------------*/
-
 void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
-//Mesma lÛgica do putSphere, mas ao invÈs de ativar o voxel, ele desativa
+//Mesma l√≥gica do putSphere, mas ao inv√©s de ativar o voxel, ele desativa
 //Desativa a esfera
   int i, j, k;
 	
@@ -155,11 +114,9 @@ void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
   }
 }
 
-/*----------------------------------------------------------------*/
-
 void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
-   //Define uma elipsoide
-   //recebe as coordenadas das dimensıes e coordenadas do centro
+  //Define uma elipsoide
+   //recebe as coordenadas das dimens√µes e coordenadas do centro
    float newx, newy, newz;
    int x, y, z;
 
@@ -169,7 +126,7 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
             newx = ((float)(x-xcenter)*(float)(x-xcenter))/(rx * rx);
             newy = ((float)(y-ycenter)*(float)(y-ycenter))/(ry * ry);
             newz = ((float)(z-zcenter)*(float)(z-zcenter))/(rz * rz);
-//calculo da condiÁ„o de existencia da elipsoid
+//calculo da condi√ß√£o de existencia da elipsoid
             if ((newx + newy + newz) < 1){
                 putVoxel(x,y,z);
             }
@@ -178,10 +135,8 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
     }
 }
 
-/*----------------------------------------------------------------*/
-
 void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
-   //Desativa a elipsoid
+ //Desativa a elipsoid
    
    double newx, newy, newz;
    int x, y, z;
@@ -201,87 +156,89 @@ void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
     }
 }
 
-/*----------------------------------------------------------------*/
+void Sculptor::writeOFF( char *filename){
+    int qtd_Voxel = 0;
+    int ref;
+    int  i, j, k, a, b, c; //auxiliares
+    int fix = 0.5;
+    std::ofstream endArq;
+    endArq.open(filename);
+    if (endArq.is_open()){
+        endArq<<"OFF\n"; //Define off na primeira linha
 
-void Sculptor::writeOFF(char *filename)
-{
-  int qtd_Voxel = 0;
-  int ref;
-  int  i, j, k, a, b, c; //auxiliares
-  ofstream endArq; //Responsavel por criar o arquivo
-  endArq.open(filename); //Cria o arquivo com o nome recebido
-  endArq<<"OFF\n"; //Define off na primeira linha
-
-  // Percorre todas as dimensoes verificando os voxel em exibiÁao
-  for (i = 0; i < nx; i++){
-      for (j = 0; j <ny; j++){
-           for (k = 0; k <nz; k++){
-                if(v[i][j][k].isOn == true){
-                qtd_Voxel++; //Guarda a quantidade de voxel ativos
+        // Percorre todas as dimensoes verificando os voxel em exibi√ßao
+        for (i = 0; i < nx; i++){
+            for (j = 0; j <ny; j++){
+                for (k = 0; k <nz; k++){
+                    if(v[i][j][k].isOn == true){
+                    qtd_Voxel++; //Guarda a quantidade de voxel ativos
+                    }
                 }
-           }
-       }
-  }
-  endArq<<qtd_Voxel * 8<<" "<<qtd_Voxel * 6 << " " << "0" << "\n"; 
-  // exibe a quantidade total de vertices, faces e arestas
+            }
+        }
+        endArq<<qtd_Voxel * 8<<" "<<qtd_Voxel * 6 << " " << "0" << "\n"; 
+        // exibe a quantidade total de vertices, faces e arestas
 
-  //Pecorre as posiÁıes a, b, c e exibe as coordenadas do voxel
-  //No primeiro voxel o fix ser· o valor de referencia
-  for (a = 0; a < nx; a++){
-      for (b = 0; b < ny; b++){
-           for (c = 0; c < nz; c++){
-                if(v[a][b][c].isOn == true){
-                endArq << a - fix << " " << b + fix << " " << c - fix << "\n" << flush;
-                endArq << a - fix << " " << b - fix << " " << c - fix << "\n" << flush;
-                endArq << a + fix << " " << b - fix << " " << c - fix << "\n" << flush;
-                endArq << a + fix << " " << b + fix << " " << c - fix << "\n" << flush;
-                endArq << a - fix << " " << b + fix << " " << c + fix << "\n" << flush;
-                endArq << a - fix << " " << b - fix << " " << c + fix << "\n" << flush;
-                endArq << a + fix << " " << b - fix << " " << c + fix << "\n" << flush;
-                endArq << a + fix << " " << b + fix << " " << c + fix << "\n" << flush;
+        //Pecorre as posi√ß√µes a, b, c e exibe as coordenadas do voxel
+        //No primeiro voxel o fix ser√° o valor de referencia
+        for (a = 0; a < nx; a++){
+            for (b = 0; b < ny; b++){
+                for (c = 0; c < nz; c++){
+                    if(v[a][b][c].isOn == true){
+                        endArq << a - fix << " " << b + fix << " " << c - fix << "\n" << flush;
+                        endArq << a - fix << " " << b - fix << " " << c - fix << "\n" << flush;
+                        endArq << a + fix << " " << b - fix << " " << c - fix << "\n" << flush;
+                        endArq << a + fix << " " << b + fix << " " << c - fix << "\n" << flush;
+                        endArq << a - fix << " " << b + fix << " " << c + fix << "\n" << flush;
+                        endArq << a - fix << " " << b - fix << " " << c + fix << "\n" << flush;
+                        endArq << a + fix << " " << b - fix << " " << c + fix << "\n" << flush;
+                        endArq << a + fix << " " << b + fix << " " << c + fix << "\n" << flush;
+                    }
                 }
-           }
-      }
-  }
+            }
+        }
 
-  qtd_Voxel = 0; //Reseta a quantidade de voxel
+        qtd_Voxel = 0; //Reseta a quantidade de voxel
 
-  // percorre todas as dimensoes
-  // Verifica os voxel ativos e exibe as linhas com numeros de faces e as combinacoes que exibem a face
-  for (a= 0; a<nx; a++){
-      for (b = 0; b<ny; b++){
-           for (c= 0; c<nz; c++){
-                if(v[a][b][c].isOn == true){
-                ref = qtd_Voxel * 8;
-                endArq << fixed;
+        // percorre todas as dimensoes
+        // Verifica os voxel ativos e exibe as linhas com numeros de faces e as combinacoes que exibem a face
+        for (a= 0; a<nx; a++){
+            for (b = 0; b<ny; b++){
+                for (c= 0; c<nz; c++){
+                    if(v[a][b][c].isOn == true){
+                    ref = qtd_Voxel * 8;
+                    endArq << fixed;
 
-                // Construir linha que monta as faces a partir do vertices:
-                //Valores exemplos do site quando ref = 0:
-                endArq << "4" << " " << 0+ref << " " << 3+ref << " " << 2+ref << " " << 1+ref << " ";
-                //exibindo propriedades do voxel:
-                endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+                    // Construir linha que monta as faces a partir do vertices:
+                    //Valores exemplos do site quando ref = 0:
+                    endArq << "4" << " " << 0+ref << " " << 3+ref << " " << 2+ref << " " << 1+ref << " ";
+                    //exibindo propriedades do voxel:
+                    endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
 
-                endArq << "4" << " " << 4+ref << " " << 5+ref << " " << 6+ref << " " << 7+ref << " ";
-                endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+                    endArq << "4" << " " << 4+ref << " " << 5+ref << " " << 6+ref << " " << 7+ref << " ";
+                    endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
 
-                endArq << "4" << " " << 0+ref << " " << 1+ref << " " << 5+ref << " " << 4+ref << " ";
-                endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+                    endArq << "4" << " " << 0+ref << " " << 1+ref << " " << 5+ref << " " << 4+ref << " ";
+                    endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
 
-                endArq << "4" << " " << 0+ref << " " << 4+ref << " " << 7+ref << " " << 3+ref << " ";
-                endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+                    endArq << "4" << " " << 0+ref << " " << 4+ref << " " << 7+ref << " " << 3+ref << " ";
+                    endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
 
-                endArq << "4" << " " << 3+ref << " " << 7+ref << " " << 6+ref << " " << 2+ref << " ";
-                endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+                    endArq << "4" << " " << 3+ref << " " << 7+ref << " " << 6+ref << " " << 2+ref << " ";
+                    endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
 
-                endArq << "4" << " " << 1+ref << " " << 2+ref << " " << 6+ref << " " << 5+ref << " ";
-                endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+                    endArq << "4" << " " << 1+ref << " " << 2+ref << " " << 6+ref << " " << 5+ref << " ";
+                    endArq << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
 
-                qtd_Voxel++; //incrementa a qtd de voxels
+                    qtd_Voxel++; //incrementa a qtd de voxels
+                    }
                 }
-           }
-       }
-  }
-  endArq.close();
+            }
+        }
+    }
+    else{
+    cout << "Erro ao abrir arquivo de texto."; 
+    }
+    
+    endArq.close();
 }
-
-
